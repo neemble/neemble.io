d800a94aa51baacb0055e928409ba9999e4057aa
CodeStarGM <codestargm@gmail.com>
2022-03-05 00:06:13 +0500

    Fix server error

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,15 +45,15 @@ function Navbar() {
             <svg
               onClick={hamburger}
               xmlns="http://www.w3.org/2000/svg"
-              class="cursor-pointer link hover:text-gray-500 h-10 w-10"
+              className="cursor-pointer link hover:text-gray-500 h-10 w-10"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
