af543b320d17990c75833e96ba8d4088c77fee60
CodeStarGM <codestargm@gmail.com>
2022-03-05 12:52:02 +0500

    Add Blur Effect

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    document.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-    });
-  }, []);
   return <Component {...pageProps} />;
 }
 
